7e8b5677b8422995204014cbd09c862e0eb60246
Jeff Frankl <jfinbox@comcast.net>
2014-02-15 12:08:25 -0500

    Spruce up the template files a little

--- templates/wordpress/footer.php
+++ templates/wordpress/footer.php
@@ -2,7 +2,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h4>Azavea</h4>
+                        <h5>Azavea</h5>
                     </div>
                 </div>
             </div>
--- templates/wordpress/index.php
+++ templates/wordpress/index.php
@@ -1,12 +1,32 @@
 <? get_header(); ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2>Index</h2>
-            <p>It's the Azavea App Template!</p>
+<section id="hero">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <h2>Beyond dots on a map</h2>
+                <p>Azavea specializes in the creation of geographic web and mobile software, as well as geospatial analysis services to enhance decision-making.
+                We are committed to working on projects with strong social value in order to promote the emergence of more vibrant and sustainable communities. 
+                Read more about what sets us apart</p>
+            </div>
         </div>
     </div>
-</div>
+</section>
+<section id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Services</h3>
+                <h4>We create software that brings together the best aspects of geography and the web.</h4>
+                <p>From the simple to the complex, we create web and mobile tools to enable the public to access and visualize your data. We also have experience building applications that can perform raster and vector calculations optimized to run across distributed servers or on GPUs and that automate models.</p>
+            </div>
+            <div class="col-md-6">
+                <h3>News</h3>
+                <p>We help make your geographic data more valuable to your organization and its stakeholders by creating web and mobile software applications. We work across a wide variety of domains including:  natural resource planning, sustainable economic development, crime analysis, real estate property analysis, redistricting, political advocacy, and cultural resources to name a few.</p>
+            </div>            
+        </div>
+    </div>
+</section>
+
 
 <? get_footer(); ?>
